cedbe1dcad6995472c7c584cf5bde192a6c8ee72
Szhao52 <szx852@gmail.com>
2025-05-21 12:17:07 +0200

    yolo4

--- test.py
+++ test.py
@@ -1,10 +1,29 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options
 
-driver = webdriver.Remote(
-    command_executor='http://localhost:4444/wd/hub',
-    options=webdriver.ChromeOptions()
-)
 
-driver.get('https://www.google.com')
-print(driver.title)
-driver.quit()
+options = Options()
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
+
+SELENIUM_REMOTE_URL = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
+
+def test_example_com_chrome():
+    driver = webdriver.Remote(
+        command_executor=SELENIUM_REMOTE_URL,
+        desired_capabilities=DesiredCapabilities.CHROME
+    )
+    driver.get("https://example.com")
+    assert "Example Domain" in driver.title
+    driver.quit()
+
+def test_example_com_firefox():
+    driver = webdriver.Remote(
+        command_executor=SELENIUM_REMOTE_URL,
+        desired_capabilities=DesiredCapabilities.FIREFOX
+    )
+    driver.get("https://example.com")
+    assert "Example Domain" in driver.title
+    driver.quit()
