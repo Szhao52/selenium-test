905b78df6662f6a4999be1b935b3886666ef2225
Szhao52 <szx852@gmail.com>
2025-05-21 12:36:15 +0200

    yolo5

--- test.py
+++ test.py
@@ -1,28 +1,29 @@
 import os
 from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
-
-
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 SELENIUM_REMOTE_URL = os.getenv("SELENIUM_REMOTE_URL", "http://localhost:4444/wd/hub")
 
 def test_example_com_chrome():
+    options = ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Remote(
         command_executor=SELENIUM_REMOTE_URL,
-        desired_capabilities=DesiredCapabilities.CHROME
+        options=options
     )
     driver.get("https://example.com")
     assert "Example Domain" in driver.title
     driver.quit()
 
 def test_example_com_firefox():
+    options = FirefoxOptions()
+    options.add_argument("--headless")
     driver = webdriver.Remote(
         command_executor=SELENIUM_REMOTE_URL,
-        desired_capabilities=DesiredCapabilities.FIREFOX
+        options=options
     )
     driver.get("https://example.com")
     assert "Example Domain" in driver.title
